e94d5966f34f639779c977a432845286bc92a7f8
shimizu_taira <taira17aug@yahoo.co.jp>
2022-12-28 16:47:01 +0900

    change

--- LouvainMethod.py
+++ LouvainMethod.py
@@ -16,20 +16,32 @@ import numpy as np
 
 from community.community_status import Status
 
-
+#パスのマックス
 __PASS_MAX = -1
+#最小値
 __MIN = 0.0000001
+#配列表示形式変更、省略なくす（np.inf : 無限大）
 np.set_printoptions(threshold=np.inf)
 
 class data:
+    """
+    fileName : ファイル名（string）
+    """
     def __init__(self, fileName):
         self.fileName = fileName
         self.information(self.fileName)
 
+    """
+    任意のグラフインスタンスを生成
+    """
     def information(self, fileName):
+        #グラフインスタンス生成
         G = nx.Graph()
+        # テキストファイルから読み取りndarray化
+        # edge : (始点,終点,重み)の3-tupleのリスト
         edge = np.genfromtxt(fileName + ".txt", delimiter=",")
 
+        # (始点,終点,重み)の3-tupleのリストから重み付きエッジをまとめて追加
         G.add_weighted_edges_from(edge)
         self.G = G
         self.edge = edge
@@ -39,7 +51,8 @@ class louvain:
     def __init__(self ,c = 0 ,dataName = "None"):
         self.c = c
         self.dataName = dataName
-        
+
+    #cのゲッター   
     def get_c(self):
         return self.c
             
@@ -90,10 +103,19 @@ def modularity(partition,graph,weight="weight"):
     
     return res
 
+"""
+    graph : グラフインスタンス
+    counter : 
+    part_init : 
+    weight : 
+    resolution : 
+    randomize : 
+"""
 def generate_dendrogram(graph,counter,c,part_init=None,weight="weight",resolution=1.,randomize=False):
+    #例外処理（有効グラフを除外）
     if graph.is_directed():
         raise TypeError("Bad graph type, use only non directed graph")
-        
+    #グラフのエッジがないとき、ノード番号をキーとして自身のノード番号を要素にもつ辞書型を返す    
     if graph.number_of_edges() == 0:
         part = dict([])
         for node in graph.nodes():
